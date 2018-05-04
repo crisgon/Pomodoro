55785d7d6e239a5bbe2b9152125f96059d6cdfa3
crisgon <kristiano.gon@gmail.com>
2018-05-03 22:46:26 -0300

    Create and styling  Layout

--- extension/popup.js
+++ extension/popup.js
@@ -1,6 +1,10 @@
 let btn = document.querySelector('[data-js="code"]');
+let circle = document.querySelector('#circle');
+let timer = 6;
 
 btn.addEventListener('click', function(e){
-  e.preventDefault();
-  alert('Clicado!');
+	e.preventDefault();
+	circle.style.animation = `fillup ${timer}s linear`;
+	console.log('ok');
 });
+
